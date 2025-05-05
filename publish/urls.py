60549e64f1ce17fa5fc43c3cc2ac8ab54350310d
unknown <Violettafox767@gmail.com>
2025-05-06 00:24:54 +0300

    upd urls

--- publish/urls.py
+++ publish/urls.py
@@ -3,8 +3,8 @@ from django.urls import re_path
 from .views import *
 
 urlpatterns = [
-    re_path(r'^$', clip_list),
-    re_path(r'^init-media$', init_media_s3),
+    re_path(r'^list$', clip_list),
+    re_path(r'^init/s3$', init_media_s3),
     re_path(r'^shuffle$', clip_list_shuffle),
     re_path(r'^clip/(?P<pk>\d+)$', clip_detail),
     re_path(r'^clip/delete/(?P<pk>\d+)$', clip_delete),
